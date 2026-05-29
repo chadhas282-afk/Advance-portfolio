40d71ce8a68109a5d75b28bb4db1c7375e22463d
chadhas282-afk <chadhas282@gmail.com>
2026-05-29 13:55:44 +0530

    add Background component with initial imports and setup

---
+++ src/components/Background.jsx
@@ -0,0 +1,5 @@
+import { useMemo, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { shaderMaterial, Stars } from '@react-three/drei';
+import * as THREE from 'three';
+import { extend } from '@react-three/fiber';
