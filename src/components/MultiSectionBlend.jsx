4c7ee4a985f642388cae0003d042c47585dd830c
chadhas282-afk <chadhas282@gmail.com>
2026-05-30 11:27:17 +0530

    feat: add initial imports for MultiSectionBlend component

---
+++ src/components/MultiSectionBlend.jsx
@@ -0,0 +1,5 @@
+import { useRef } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
+import { shaderMaterial, useScroll } from '@react-three/drei';
+import * as THREE from 'three';
+import { extend } from '@react-three/fiber';
