7c7d293dcfb02c9744114b16967f759b7940fa8f
chadhas282-afk <chadhas282@gmail.com>
2026-05-30 11:28:26 +0530

    feat: add random function to MultiSectionBlendMaterial shader

--- src/components/MultiSectionBlend.jsx
+++ src/components/MultiSectionBlend.jsx
@@ -30,3 +30,7 @@ const MultiSectionBlendMaterial = shaderMaterial(
     uniform vec3 uColor3;
   uniform vec3 uColor4;
   uniform vec3 uColor5;
+
+  float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+  }
