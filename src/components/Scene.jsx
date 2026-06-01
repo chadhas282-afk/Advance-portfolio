c48e59a36982708d2056a950ae647870b72a3316
chadhas282-afk <chadhas282@gmail.com>
2026-06-01 10:31:30 +0530

    feat: add Scene component with essential imports for 3D rendering

---
+++ src/components/Scene.jsx
@@ -0,0 +1,5 @@
+import { useRef, useLayoutEffect } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useScroll, Float, MeshTransmissionMaterial } from '@react-three/drei';
+import gsap from 'gsap';
+import * as THREE from 'three';
