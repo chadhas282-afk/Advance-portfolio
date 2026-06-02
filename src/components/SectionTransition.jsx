5d60439c1bc299b4b4fc7c5343cdf1b1e0c31298
chadhas282-afk <chadhas282@gmail.com>
2026-06-02 10:46:50 +0530

    feat: add random noise calculations to LiquidTransitionMaterial shader

--- src/components/SectionTransition.jsx
+++ src/components/SectionTransition.jsx
@@ -32,3 +32,8 @@ const LiquidTransitionMaterial = shaderMaterial(
   float noise(vec2 st) {
     vec2 i = floor(st);
     vec2 f = fract(st);
+
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
